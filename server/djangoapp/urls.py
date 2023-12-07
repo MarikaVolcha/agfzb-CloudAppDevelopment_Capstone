6011bdeb3a475543f85a43e30dcea7ab677383e0
MarikaVolcha <101819518+MarikaVolcha@users.noreply.github.com>
2023-12-07 10:35:34 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,18 +6,19 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
+    path(route='./', view=views.django, name='django'),
+    # name the URL # if try this -> do tis 
 
     # path for about view
-
+    path(route='about/', view=views.about, name='about'),
     # path for contact us view
-
+    path(route='contact', view=views.contact, name='contact'),
     # path for registration
-
-    # path for login
-
+    path(route='registration', view=views.registration_request, name='registration'),
+    # path for login 
+    path(route='login', view=views.login_request, name='login'),
     # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
